657af6cc1afdfa66c3cabf91217a6c1c7b1d2f55
devranaydinoglu <d.s.aydinoglu@gmail.com>
2023-09-09 12:27:50 +0200

    Added -rec flag for recursively getting the total size of a directory

--- src/commands/info/InfoCommand.cpp
+++ src/commands/info/InfoCommand.cpp
@@ -4,10 +4,15 @@
 #include <sys/errno.h>
 #include <sys/stat.h>
 #include <pwd.h>
+#include <filesystem>
 
 #include "InfoCommand.h"
 #include "../../utils/Math.h"
 #include "../../printer/Printer.h"
+
+using DirIterator = std::filesystem::directory_iterator;
+using RecDirIterator = std::filesystem::recursive_directory_iterator;
+using Path = std::filesystem::path;
 
 InfoCommand::InfoCommand(int argc, char** argv)
     : Command(argc, argv)
@@ -15,7 +20,7 @@ InfoCommand::InfoCommand(int argc, char** argv)
     commandInfo.name = "info";
     commandInfo.description = "Show info about the specified file.";
     commandInfo.numArgs = 1;
-    commandInfo.numFlags = 0;
+    commandInfo.numFlags = 1;
 }
 
 void InfoCommand::execute()
@@ -51,12 +56,60 @@ CommonFileInfo InfoCommand::setFileInfo(const struct stat& fileStat)
     info.numLinks = std::to_string(static_cast<int>(fileStat.st_nlink));
     // Get number of hard links to the file
     info.owner = getpwuid(fileStat.st_uid)->pw_name ? getpwuid(fileStat.st_uid)->pw_name : "-";
+
+    // Get file name from args
+    std::string fileName;
+    Path filePath;
+
+    for (const auto& entry : DirIterator(std::filesystem::current_path(), std::filesystem::directory_options::skip_permission_denied))
+    {
+        std::string tempFileName = entry.path().filename();
+
+        // Convert file name to all uppercases and lowercases to support uppercase and lowercase user input
+        std::string fileNameUpper = tempFileName;
+        std::transform(fileNameUpper.begin(), fileNameUpper.end(), fileNameUpper.begin(), ::toupper);
+        std::string fileNameLower = tempFileName;
+        std::transform(fileNameLower.begin(), fileNameLower.end(), fileNameLower.begin(), ::tolower);
+
+        std::string argsUpper = args[0];
+        std::transform(argsUpper.begin(), argsUpper.end(), argsUpper.begin(), ::toupper);
+
+        if (tempFileName == args[0] || fileNameLower == args[0] || fileNameUpper == argsUpper)
+        {
+            fileName = tempFileName;
+            filePath = entry.path();
+        }
+    }
+
+    info.name = fileName;
+
     // Get file size in bytes or number of bytes allocated to directory
-    info.size = std::to_string(static_cast<int>(fileStat.st_size));
+    off_t totalSize = 0;
+    mode_t perm = fileStat.st_mode;
+
+    if (S_ISDIR(perm) && containsFlag("-rec"))
+    {
+        for (const auto& entry : RecDirIterator(filePath, std::filesystem::directory_options::skip_permission_denied))
+        {
+            struct stat fileStat;
+
+            // Check if valid file info has been returned
+            if (stat(entry.path().c_str(), &fileStat) != 0)
+            {
+                break;
+            }
+            totalSize += fileStat.st_size; 
+        }
+    }
+    else
+    {
+        totalSize = fileStat.st_size;
+    }
+
+    info.size = std::to_string(totalSize);
+
     // Get time and date of last modification
     info.lastModified = Command::getLastModified(fileStat);
-    // Get file name from args
-    info.name = args[0];
 
     return info;
 }
