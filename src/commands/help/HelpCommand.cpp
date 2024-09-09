4a247f2b6d2341b02ecbc0b80423758a0c0dd6c3
devranaydinoglu <d.s.aydinoglu@gmail.com>
2024-09-09 19:38:46 +0200

    Updated info for list, find, and cd commands

--- src/commands/help/HelpCommand.cpp
+++ src/commands/help/HelpCommand.cpp
@@ -28,16 +28,16 @@ void HelpCommand::execute()
     Printer::print("Show info about the specified file.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
     std::cout << "\n\n";
     Printer::print("`ogy ls (-all) (-rec) (-mt)` - ", 0, TextColor::WHITE, TextEmphasis::BOLD);
-    Printer::print("List info about items in the current directory. Include the `-all` flag to include hidden items. Include the `-mt` flag to use mutlithreading.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
+    Printer::print("List info about items in the current directory. Include the `-all` flag to include hidden items. Include the `-rec` flag to recursively iterate through all subdirectories to get its total size. Include the `-mt` flag to use multithreading.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
     std::cout << "\n\n";
     Printer::print("`ogy find {term} (-rec)` - ", 0, TextColor::WHITE, TextEmphasis::BOLD);
-    Printer::print("Find all files in the current directory which include `term` in their file name.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
+    Printer::print("Find file(s) in the current directory containing the specified term.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
     std::cout << "\n\n";
     Printer::print("`ogy cd {alias} {path}` - ", 0, TextColor::WHITE, TextEmphasis::BOLD);
-    Printer::print("Change directory. If the alias exists, go to its corresponding path. Otherwise store the path as the alias in the config file and go to the specified path. Alias is optional, so it could also be used as the built-in cd command.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
-    std::cout << "\n\n";
-    Printer::print("`ogy create {template name}` - ", 0, TextColor::WHITE, TextEmphasis::BOLD);
-    Printer::print("Create a new project directory based on the project structure specified in the config file.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
+    Printer::print("Change directory to an alias' corresponding path. If the alias exists, go to its corresponding path. Otherwise store the path as the alias in the config file and go to the specified path. (Alias is optional, so it could also be used as the built-in cd command)", 0, TextColor::WHITE, TextEmphasis::NORMAL);
+    Printer::print("\nIMPORTANT: ", 0, TextColor::YELLOW, TextEmphasis::BOLD);
+    Printer::print("The alias cannot contain forward slashes.", 0, TextColor::WHITE, TextEmphasis::NORMAL);
+    
     std::cout << "\n\n";
     Printer::print("Author: devran", 0, TextColor::GRAY, TextEmphasis::BOLD);
     Printer::print("\n\n------------------------------", 0, TextColor::WHITE, TextEmphasis::BOLD);
